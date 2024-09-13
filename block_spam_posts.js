9117214bbdc8a835cc2546dd80b0cbabda7e8d93
Alex <raposoalexander@gmail.com>
2024-09-13 12:50:50 -0400

    Modified block_spam_posts.js to use an observer to run our code whenever the URL changes

--- block_spam_posts.js
+++ block_spam_posts.js
@@ -1,23 +1,17 @@
 const filter_spam_posts = (blacklisted_words) => {
-
     const posts = document.querySelectorAll(".forum_topic");
-    let posts_removed_count = 0;
-    
     posts.forEach((post) => {
-
         const post_title = post.querySelector(".forum_topic_name").textContent.toLowerCase();
-        const post_content_preview = post.getAttribute("data-tooltip-forum");
-
-        if (blacklisted_words.some(word => post_title.includes(word.toLowerCase())) || blacklisted_words.some(word => post_content_preview.includes(word))) {
+        const post_content_preview = post.getAttribute("data-tooltip-forum").toLowerCase();
+        if (blacklisted_words.some(word => post_title.includes(word.toLowerCase())) || blacklisted_words.some(word => post_content_preview.includes(word.toLowerCase()))) {
             post.remove();
-            ++posts_removed_count;
         }
-
     });
-    
-    return posts_removed_count;
-
 };
 
-const blacklisted_words = ["spam", "advertisement", "offensive"]; // Replace it w whatever words you wish to filter out
-console.log(`Hid ${filter_spam_posts(blacklisted_words)} spam post(s)!`);
+const blacklisted_words = ["spam", "woke", "clown", "bait", "advertisement", "offensive", "denuvo", "DRM", "SJW", "SBI", "identity", "identify", "palestine", "spyware", "ccp", 
+                           "homosexual", "homophobic", "banned", "censorship", "racist", "racism", "transgender", "transsexual", "transphobic", "jester", "gud", "sexist", "semetic"];
+// Observer will look for changes in the document bodys child list indicating a URL change
+const observer = new MutationObserver(() => { filter_spam_posts(blacklisted_words); }); 
+const observer_config = { childList: true };
+observer.observe(document.body, observer_config);
